3422da2f99c28d8e71ae36de9ffd55b51ea1dd68
Your Name <you@example.com>
2023-03-02 02:53:01 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,39 +1,39 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string
- * @str: the string to be capitalized.
- *
- * Return: a pointer to the changed string.
+ * cap_string - capitalize all words of a string
+ * @str: string
+ * Return: `str`
  */
 
 char *cap_string(char *str)
 {
-	int alpha = 0;
+	int i, c;
+	int trigger;
+	char nots[] = ",;.!?(){}\n\t\" ";
 
-	while (str[alpha])
+	for (i = 0, trigger = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[alpha] >= 'a' && str[alpha] <= 'z'))
-			alpha++;
+		if (str[0] > 96 && str[0] < 123)
+			trigger = 1;
+		for (c = 0; nots[c] != '\0'; c++)
+		{
+			if (nots[c] == str[i])
+				trigger = 1;
+		}
 
-		if (str[alpha - 1] == ' ' ||
-		    str[alpha - 1] == '\t' ||
-		    str[alpha - 1] == '\n' ||
-		    str[alpha - 1] == ',' ||
-		    str[alpha - 1] == ';' ||
-		    str[alpha - 1] == '.' ||
-		    str[alpha - 1] == '!' ||
-		    str[alpha - 1] == '?' ||
-		    str[alpha - 1] == '"' ||
-		    str[alpha - 1] == '(' ||
-		    str[alpha - 1] == ')' ||
-		    str[alpha - 1] == '{' ||
-		    str[alpha - 1] == '}' ||
-		    alpha == 0)
-			str[alpha] -= 32;
-
-		alpha++;
+		if (trigger)
+		{
+			if (str[i] > 96 && str[i] < 123)
+			{
+				str[i] -= 32;
+				trigger = 0;
+			}
+			else if (str[i] > 64 && str[i] < 91)
+				trigger = 0;
+			else if (str[i] > 47 && str[i] < 58)
+				trigger = 0;
+		}
 	}
-
 	return (str);
 }
