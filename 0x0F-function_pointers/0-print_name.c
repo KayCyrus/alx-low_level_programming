fda660253ab6238af6d840662f17c6134f287eca
KojoKay <kojokay1904@gmail.com>
2023-03-22 17:26:37 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
-#include "function_pointers.h"
-
 /**
- * print_name - a function that prints a name.
- * @name: the string to be printed
- * @f: array of pointer function
- */
+ * print_name - function to print name
+ *
+ * @name: name to print
+ * @f: function pointer that doesn't return anything
+ *
+ * Return: Always 0 (Success)
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
